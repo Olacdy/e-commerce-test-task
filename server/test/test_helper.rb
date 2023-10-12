9e3c0faaddd2ee7c3cdeb52ec676281ff64e6d85
Олег Дидечкин <crm.29431@dz.com>
2023-10-12 14:47:10 +0300

    Docker with postgresql as a db

--- server/test/test_helper.rb
+++ server/test/test_helper.rb
@@ -1,15 +1,19 @@
 ENV["RAILS_ENV"] ||= "test"
 require_relative "../config/environment"
 require "rails/test_help"
+require "helpers/doorkeeper_params"
 
-module ActiveSupport
-  class TestCase
-    # Run tests in parallel with specified workers
-    parallelize(workers: :number_of_processors, with: :threads)
+class ActiveSupport::TestCase
+  include FactoryBot::Syntax::Methods
+  include Helpers::DoorkeeperParams
 
-    # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-    fixtures :all
+  # Run tests in parallel with specified workers
+  parallelize(workers: :number_of_processors)
+end
 
-    # Add more helper methods to be used by all tests here...
+Shoulda::Matchers.configure do |config|
+  config.integrate do |with|
+    with.test_framework :minitest
+    with.library :rails
   end
 end
