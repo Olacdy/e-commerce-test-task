dfd1ae682c00d65a6caa54e22013c32bb0d12060
Олег Дидечкин <crm.29431@dz.com>
2023-10-19 14:07:30 +0300

    Empty cart page

---
+++ client/src/components/orders/cart.tsx
@@ -0,0 +1,112 @@
+import { Dispatch, FC, SetStateAction } from 'react';
+
+import { toast } from 'sonner';
+
+import snakecaseKeys from 'snakecase-keys';
+
+import { Button } from '@/components/ui/button';
+
+import OrderDescription from '@/components/orders/order-description';
+
+import useCartStore from '@/context/cart-context';
+import useTokenStore from '@/context/token-context';
+
+import { getApiUrl } from '@/lib/utils';
+
+import { OrderType } from '@/schemas/order-schemas';
+import { Link } from 'react-router-dom';
+
+type CartProps = {
+  setOrders: Dispatch<SetStateAction<OrderType[]>>;
+};
+
+const Cart: FC<CartProps> = ({ setOrders }) => {
+  const token = useTokenStore((state) => state.token);
+  const { total, cart, deleteFromCart, clearCart } = useCartStore((state) => ({
+    total: state.total(),
+    cart: state.cart,
+    deleteFromCart: state.deleteFromCart,
+    clearCart: state.clearCart,
+  }));
+
+  const handleOrderConfirm = async () => {
+    const requestOptions = {
+      method: 'POST',
+      headers: {
+        Accept: '*/*',
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify(
+        snakecaseKeys({
+          order: {
+            orderDescriptionsAttributes: cart.map((ct) => ({
+              itemId: ct.item.id,
+              quantity: ct.quantity,
+            })),
+          },
+        })
+      ),
+    };
+
+    try {
+      const response = await fetch(`${getApiUrl()}/orders`, requestOptions);
+
+      if (!response.ok) throw new Error(`${response.status}`);
+
+      const data = await response.json();
+
+      clearCart();
+
+      setOrders((prev) => [
+        { ...data, createdAt: new Date(data.createdAt) },
+        ...prev,
+      ]);
+
+      toast.success('Order created.');
+    } catch (error) {
+      toast.error('Something went wrong. Try again later.');
+    }
+  };
+
+  return (
+    <>
+      {cart.length > 1 ? (
+        <>
+          <OrderDescription
+            orderDescriptions={cart}
+            handleDeleteFromCart={deleteFromCart}
+          />
+
+          <div className='flex flex-col-reverse items-end justify-between gap-5 sm:flex-row sm:items-center'>
+            <Button
+              variant='outline'
+              onClick={clearCart}
+              className='whitespace-nowrap text-base'>
+              Clear Cart
+            </Button>
+            <div className='flex items-center gap-5'>
+              <p className='text-xl'>
+                Total: <span>{total}</span>
+              </p>
+              <Button onClick={handleOrderConfirm} className='px-7 text-base'>
+                Confirm
+              </Button>
+            </div>
+          </div>
+        </>
+      ) : (
+        <div className='flex w-full flex-col items-center gap-12 pt-20'>
+          <h2 className='text-3xl md:text-5xl'>Cart is empty.</h2>
+          <Link to='/'>
+            <Button variant='link' className='text-lg md:text-2xl'>
+              Try to add something
+            </Button>
+          </Link>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Cart;
--- client/src/components/orders/orders.tsx
+++ client/src/components/orders/orders.tsx
@@ -2,12 +2,8 @@ import { useEffect, useState } from 'react';
 
 import { toast } from 'sonner';
 
-import snakecaseKeys from 'snakecase-keys';
-
-import { Button } from '@/components/ui/button';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
 
-import OrderDescription from '@/components/orders/order-description';
 import OrdersTable from '@/components/orders/orders-table';
 
 import useCartStore from '@/context/cart-context';
@@ -15,19 +11,13 @@ import useTokenStore from '@/context/token-context';
 
 import { getApiUrl } from '@/lib/utils';
 
-import {
-  OrderDescriptionType,
-  OrderType,
-  ordersSchema,
-} from '@/schemas/order-schemas';
+import Cart from '@/components/orders/cart';
+import { OrderType, ordersSchema } from '@/schemas/order-schemas';
 
 const Orders = () => {
   const token = useTokenStore((state) => state.token);
-  const { cart, total, deleteFromCart, clearCart } = useCartStore((state) => ({
+  const { cart } = useCartStore((state) => ({
     cart: state.cart,
-    total: state.total(),
-    deleteFromCart: state.deleteFromCart,
-    clearCart: state.clearCart,
   }));
 
   const [tabValue, setTabValue] = useState<'cart' | 'orders'>(
@@ -68,59 +58,6 @@ const Orders = () => {
     if (orders.length < 1) fetchOrders();
   }, [token, orders.length]);
 
-  const handleOrderConfirm = async () => {
-    const requestOptions = {
-      method: 'POST',
-      headers: {
-        Accept: '*/*',
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${token}`,
-      },
-      body: JSON.stringify(
-        snakecaseKeys({
-          order: {
-            orderDescriptionsAttributes: cart.map((ct) => ({
-              itemId: ct.item.id,
-              quantity: ct.quantity,
-            })),
-          },
-        })
-      ),
-    };
-
-    try {
-      const response = await fetch(`${getApiUrl()}/orders`, requestOptions);
-
-      if (!response.ok) throw new Error(`${response.status}`);
-
-      const data = await response.json();
-
-      clearCart();
-
-      setTabValue('orders');
-
-      setOrders((prev) => [
-        { ...data, createdAt: new Date(data.createdAt) },
-        ...prev,
-      ]);
-
-      toast.success('Order created.');
-    } catch (error) {
-      toast.error('Something went wrong. Try again later.');
-    }
-  };
-
-  const handleClearCart = () => {
-    clearCart();
-    setTabValue('orders');
-  };
-
-  const handleDeleteFromCart = (cartItem: OrderDescriptionType) => {
-    deleteFromCart(cartItem);
-
-    if (cart.length === 1) setTabValue('orders');
-  };
-
   return (
     <section className='flex w-full max-w-lg flex-col px-10 pb-10 md:max-w-3xl lg:max-w-6xl'>
       <Tabs
@@ -128,36 +65,14 @@ const Orders = () => {
         onValueChange={handleTabValueChange}
         className='w-full'>
         <TabsList className='grid w-full max-w-xs grid-cols-2'>
-          <TabsTrigger disabled={cart.length < 1} value='cart'>
-            Cart
-          </TabsTrigger>
+          <TabsTrigger value='cart'>Cart</TabsTrigger>
           <TabsTrigger value='orders'>Orders</TabsTrigger>
         </TabsList>
         <TabsContent value='orders'>
           <OrdersTable orders={orders} />
         </TabsContent>
         <TabsContent value='cart' className='flex flex-col gap-32 pt-12'>
-          <OrderDescription
-            orderDescriptions={cart}
-            handleDeleteFromCart={handleDeleteFromCart}
-          />
-
-          <div className='flex flex-col-reverse items-end justify-between gap-5 sm:flex-row sm:items-center'>
-            <Button
-              variant='outline'
-              onClick={handleClearCart}
-              className='whitespace-nowrap text-base'>
-              Clear Cart
-            </Button>
-            <div className='flex items-center gap-5'>
-              <p className='text-xl'>
-                Total: <span>{total}</span>
-              </p>
-              <Button onClick={handleOrderConfirm} className='px-7 text-base'>
-                Confirm
-              </Button>
-            </div>
-          </div>
+          <Cart setOrders={setOrders} />
         </TabsContent>
       </Tabs>
     </section>
